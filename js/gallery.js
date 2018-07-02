95c9fc5ad1f2f120f8f7c221c47e9c6f2712df7b
Vasylenko Nika <vslnknika@gmail.com>
2018-07-02 16:01:02 +0300

    delete function

--- js/gallery.js
+++ js/gallery.js
@@ -12,15 +12,12 @@
 let showImg = document.querySelector('.active');
 function InitializeGallery(){
     this.images = document.querySelectorAll(".fotos img");
-    this.initialize = function(){
         this.images.forEach((item, i)=>{
             item.addEventListener("mouseover", function(){
                 showImg.src = item.dataset.bigImage; 
             })
             console.log(item.dataset.bigImage);
         })
-    } 
 }
 
 let initializePhoto = new InitializeGallery();
-initializePhoto.initialize();
